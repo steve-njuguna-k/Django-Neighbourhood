e06d54e41dd0d89f0e6e9369c677081cb2fc103d
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-16 13:35:16 +0300

    Added Auth URLs

--- Neighbourhood/urls.py
+++ Neighbourhood/urls.py
@@ -4,5 +4,8 @@ from django.conf.urls.static import static
 from Core import settings
 
 urlpatterns = [
-    path('', views.Home, name='Home')
+    path('', views.Home, name="Home"),
+    path('register', views.Register, name="Register"),
+    path('login', views.Login, name="Login"),
+    path('logout', views.Logout, name="Logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
